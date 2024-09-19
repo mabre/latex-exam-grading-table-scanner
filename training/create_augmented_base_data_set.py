2f8fe577f6b94b67a2eaa43aeff7075a01c5eeb3
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2024-09-19 12:54:59 +0200

    add rotation/scaling to preprocessing

--- training/create_augmented_base_data_set.py
+++ training/create_augmented_base_data_set.py
@@ -8,16 +8,24 @@ import numpy as np
 
 DIGIT_IMAGE_SIZE = 32
 
-def scale_image_with_border(image: np.array, target_size: int = 32) -> np.array:
+def scale_image_with_border(image: np.array, target_size: int = DIGIT_IMAGE_SIZE, min_scale: float = 0.7, max_scale: float = 1.0) -> np.array:
+    """
+    input is an image with a handwritten digit (nothing else, no lines etc.) of any size
+    the following operations are applied
+    - randomly scale down a bit
+    - add white padding to get target size
+    """
     h, w = image.shape[:2]
     aspect_ratio = w / h
 
+    scale_factor = random.uniform(min_scale, max_scale)
+
     if aspect_ratio > 1:
-        new_w = target_size
-        new_h = int(target_size / aspect_ratio)
+        new_w = int(target_size * scale_factor)
+        new_h = int(target_size / aspect_ratio * scale_factor)
     else:
-        new_h = target_size
-        new_w = int(target_size * aspect_ratio)
+        new_h = int(target_size * scale_factor)
+        new_w = int(target_size * aspect_ratio * scale_factor)
 
     resized_image = cv2.resize(image, (new_w, new_h))
     result = np.ones((target_size, target_size, 3), dtype=np.uint8) * 255
@@ -65,20 +73,37 @@ def add_random_lines(image: np.array) -> np.array:
     return image
 
 
+def randomly_rotate_image(image: np.array, max_angle: int = 5) -> np.array:
+    h, w = image.shape[:2]
+    angle = random.uniform(-max_angle, max_angle)
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated_image = cv2.warpAffine(image, M, (w, h), borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
+    return rotated_image
+
+
 def process_and_save_images(input_path: Path, output_path: Path):
+    # make sure we have a balanced data set in the end
+    files_to_create_per_digit = min(len(os.listdir(input_path / str(digit))) for digit in range(10))
+
     for digit in range(10):
         digit_path = input_path / str(digit)
 
-        for filename in tqdm(os.listdir(digit_path), desc=f"Processing digit {digit}"):
+        filenames = sorted(os.listdir(digit_path))[:files_to_create_per_digit]
+        for filename in tqdm(filenames, desc=f"Processing digit {digit}"):
             if filename.lower().endswith('.png'):
                 image_path = digit_path / filename
                 image = cv2.imread(image_path)
 
                 scaled_image = scale_image_with_border(image)
                 moved_image = randomly_move_image(scaled_image)
-                final_image = add_random_lines(moved_image)
+                image_with_lines = add_random_lines(moved_image)
+                final_image = randomly_rotate_image(image_with_lines)
 
                 processed_image_path = output_path / str(digit) / filename
                 cv2.imwrite(processed_image_path, final_image)
 
+random.seed(0)
+np.random.seed(0)
+# TODO path by parameters
 process_and_save_images(Path('../../abschlussarbeiten/pa-mersch/numbers/UNCATEGORIZED'), Path('../corpus/UNCAT_AUGMENTED'))
