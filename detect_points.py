98f8dde6c61a5d26e8883c5637b30f59f7514b86
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2025-02-20 14:58:14 +0100

    also try threasholing oif no qr code found
    Wenn das licht nicht optimal ist, finden wir damit eher noch einen Code. Im live-capture ist das schnell genug, wenn nicht live rufen wir die Funktion woeiso nur auf, wenn wir die arucos haben

--- detect_points.py
+++ detect_points.py
@@ -237,6 +237,11 @@ def student_number_from_qr_code(image: np.array) -> Tuple[Optional[str], Optiona
 def read_qr_code(image: np.array) -> Tuple[Any, Optional[np.array]]:
     qr_decoder = cv2.QRCodeDetector()
     data, points, _ = qr_decoder.detectAndDecode(image)
+    if points is not None:
+        return data, points
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    data, points, _ = qr_decoder.detectAndDecode(binary)
     return data, points
 
 
