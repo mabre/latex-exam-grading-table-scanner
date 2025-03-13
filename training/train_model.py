e9ec5a27b9c25450377290230cdd0b89e0032c1d
Markus Brenneis <Markus.Brenneis@uni-duesseldorf.de>
2025-03-13 19:10:55 +0100

    try le-net like architecture

--- training/train_model.py
+++ training/train_model.py
@@ -8,11 +8,9 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from keras.api.callbacks import ModelCheckpoint
-from keras.api.layers import BatchNormalization
-from keras.api.layers import Conv2D, MaxPooling2D
-from keras.api.layers import Dense, Flatten
 from keras.api.models import Sequential
 from sklearn.model_selection import train_test_split
+from keras import layers, models
 from tqdm import tqdm
 
 from constants import DIGIT_IMAGE_SIZE
@@ -131,32 +129,18 @@ def generate_datasets(train_images: np.array, train_labels: np.array, test_image
 
 
 def generate_model() -> Sequential:
-    model = Sequential()
-    model.add(
-        Conv2D(
-            filters=64,
-            kernel_size=(3, 3),
-            activation="relu",
-            input_shape=(DIGIT_IMAGE_SIZE, DIGIT_IMAGE_SIZE, 1),
-        )
-    )
-    model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation="relu"))
-    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Conv2D(filters=512, kernel_size=(3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(BatchNormalization())
-    model.add(Dense(512, activation="relu"))
-    model.add(Dense(10, activation="softmax"))
+    model = models.Sequential([ # based on lenet, with dropout
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(DIGIT_IMAGE_SIZE, DIGIT_IMAGE_SIZE, 1)),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.25),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Dropout(0.25),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(10, activation='softmax')
+    ])
     model.compile(
         optimizer=tf.keras.optimizers.Adam(
             learning_rate=3e-5,
